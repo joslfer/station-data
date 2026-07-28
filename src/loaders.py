85c7ba3d38aeefc5f7a50688c8725d0db44570fa
joslfer <jose.luefer@gmail.com>
2026-07-28 18:17:31 +0200

    adds month loader and range loader

---
+++ src/loaders.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+def load_month(year,month, data_dir = "../data"):
+    path = f"{data_dir}/A-{year}/F{year}-{month:02d}.xls" # 02d fills with zeros unitl 2 digits
+    df_raw = pd.read_excel(path)
+
+    # separating total summary rows
+    # past logic:  
+    # mask_total = (df_raw["Año"]=="Total") 
+    # df = df_raw[~mask_total]
+    mask = (df_raw["Año"]==year) & (df_raw["Mes"] == month)
+    df = df_raw[mask]
+
+    # droping usless columns
+    cols_to_drop = ['Hora', 'T. máx.', 'Hora.1', 'T. mín.', 'Hora.2', 'HR. mx.', 'Hora.3', 'HR. mn.', 'Hora.4']
+    df = df.drop(columns= cols_to_drop)
+
+    #renaming
+    rename_map = {
+        "Año": "year",
+        "Mes": "month",
+        "Día": "day",
+        "H": "H",
+        "P (mm)": "rain_mm",
+        "T (°C)": "temp",
+        "HR %": "humidity",
+        "Tens. V": "vapor_pressure",
+        "Rocío": "dew_point",
+    }
+    df = df.rename(columns=rename_map)
+
+    # parsing H values
+    def parse_hour(raw_value):
+        chain = str(int(raw_value)).zfill(4)
+        hours = chain[:2]
+        minutes = chain[2:]
+        return f"{hours}:{minutes}"
+
+    df["H"] = df["H"].apply(parse_hour)
+
+    # changing 24:00 to 00:00 (pending: adding a day)
+    mask_2400 = df["H"] == "24:00"
+    df.loc[mask_2400, "H"] = "00:00"
+
+    # creating a timestamp
+    df["hour"] = df["H"].str[:2]
+    df["minute"] = df["H"].str[3:5]
+    df["second"] = 0
+    df["date"]=pd.to_datetime(df[["year","month","day","hour","minute","second"]])
+
+    # changing the order 
+    new_order = [
+    "date",
+    "temp",
+    "humidity",
+    "vapor_pressure",
+    "dew_point",
+    "rain_mm",
+    "H",
+    "year",
+    "month",
+    "day",
+    "hour",
+    "minute",
+    "second"
+    ]
+
+    df = df[new_order]
+
+    # adding a day
+    df.loc[mask_2400,"date"] += pd.Timedelta(days=1)
+
+    return df
+
+
+
+def load_range(start_year, start_month, end_year, end_month, data_dir = "../data"):
+    monthly_dfs = []
+    start = f"{start_year}-{start_month:02d}-01"
+    end = f"{end_year}-{end_month:02d}-01"
+    months = pd.date_range(start,end,freq = "MS")
+    for i in months:
+    #try:
+        year = i.year
+        month = i.month 
+        monthly_dfs.append(load_month(year, month))
+    #except Exception as e:
+        #print(f"Error loading {month} {year}")
+        #raise
+    combined = pd.concat(monthly_dfs).reset_index(drop = True)
+    return combined
